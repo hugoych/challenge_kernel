a415e6a5d6e9fcf2914e401bb19ab81d0d857bb8
Avalos <raphael.avalos@telecom-paristech.fr>
2019-03-13 14:07:56 +0100

    add doc

--- kernel_methods.py
+++ kernel_methods.py
@@ -32,7 +32,21 @@ class Hyper:
 
 
 class SVM:
+    '''
+    Class to perform SVM adapted for Hyper
+    To add a new kernel:
+    If possible create a method _new_kernel(self, X=None) to use matrix product for computation efficiency
+    Else create a callable that take into parameter x_i, x_j and returns K(x_i, x_j).
+    '''
     def __init__(self, kernel, C=None, verbose=True, kernel_param=None):
+        '''
+
+        Args:
+            kernel (string or callable): Either a string between 'gaussian', 'polynomial', 'spectrum' or a callable
+            C (float): Parameter for SVC
+            verbose (bool): To debug
+            kernel_param (dict): Dict containing the parameters for the kernel
+        '''
         self.kernel = None
         self.set_kernel(kernel)
         self.kernel_param = kernel_param
@@ -45,6 +59,7 @@ class SVM:
         self.Y = None
         self.k = kernel_param.get('k', 3)
         self.X_spectrum = None
+        # spectrum_dict: only for spectrum kernel
         self._spectrum_dict = {e: i for i, e in enumerate(itertools.product(*[range(4)] * self.k))}
 
     def reset(self, kernel, C=None, verbose=True, kernel_param=None):
@@ -63,10 +78,18 @@ class SVM:
         self._spectrum_dict = {e: i for i, e in enumerate(itertools.combinations_with_replacement(range(4), self.k))}
 
     def set_kernel(self, kernel):
+        '''
+        Set a new kernel (doesn't reset the spectrum nor the gramm matrix)
+        Args:
+            kernel (str or callable):  Either a string between 'gaussian', 'polynomial', 'spectrum' or a callable
+        '''
         assert (kernel in ['gaussian', 'polynomial', 'spectrum']) or callable(kernel)
         self.kernel = kernel
 
     def solve(self):
+        '''
+        Solve the Convex optimization problem
+        '''
         n, d = self.X.shape
         P = cvxopt.matrix(self.K)
         y = self.Y.astype(np.float64)
@@ -89,6 +112,13 @@ class SVM:
             self.X_spectrum = self.X_spectrum[mask]
 
     def build_K(self, X=None):
+        '''
+        Build the K matrix if X is none it corresponds to the Gramm Matrix
+        Args:
+            X (np.array or None):
+
+        Returns: (np.array) the K matrix
+        '''
         if not callable(self.kernel):
             if self.kernel == 'gaussian':
                 return self._gaussian_kernel(X)
@@ -116,6 +146,14 @@ class SVM:
             return K
 
     def _gaussian_kernel(self, X=None):
+        '''
+        Compute the K matrix with the Gaussian kernel
+        Args:
+            X (np.array or None):
+
+        Returns: (np.array) the K matrix
+
+        '''
         var = self.kernel_param.get('var', 5.0)
         if X is None:
             XtX = self.X @ self.X.T
@@ -130,6 +168,14 @@ class SVM:
         return K
 
     def _polynomial_kernel(self, X=None):
+        '''
+        Compute the K matrix with the polynomial kernel
+        Args:
+            X (np.array or None):
+
+        Returns:(np.array) the K matrix
+
+        '''
         p = self.kernel_param.get('p', 3)
         if X is None:
             XtX = self.X @ self.X.T
@@ -156,15 +202,39 @@ class SVM:
         return K
 
     def fit(self, X, Y):
+        '''
+        Fit the SVC
+        Args:
+            X (np.array): input
+            Y (np.array): target
+
+        '''
         self.X = X
         self.Y = Y
         self.K = self.build_K()
         self.solve()
 
     def predict(self, X):
+        '''
+        Make prediction
+        Args:
+            X (np.array): input
+
+        Returns: (np.array) target
+
+        '''
         K = self.build_K(X)
         y = K.T @ self.alpha
         return y
 
     def score(self, X, Y):
+        '''
+        Score the SVC
+        Args:
+            X (np.array): input
+            Y (np.array): target
+
+        Returns: (float) score
+
+        '''
         return np.mean(np.sign(self.predict(X).reshape((-1, 1))) == Y.reshape((-1, 1)))
